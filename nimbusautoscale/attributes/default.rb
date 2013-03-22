27aa29e6a411683f0286cbc4d0f27c7e219b6707
Pierre Riteau <priteau@ci.uchicago.edu>
2013-03-22 23:26:58 +0100

    Add statsd block to the Phantom config file

--- nimbusautoscale/attributes/default.rb
+++ nimbusautoscale/attributes/default.rb
@@ -1,3 +1,5 @@
 set_unless[:virtualenv][:python] = "python"
 set_unless[:virtualenv][:virtualenv] = "virtualenv"
 set_unless[:autoscale][:exceptional_api_key] = nil
+set_unless[:autoscale][:statsd][:host] = nil
+set_unless[:autoscale][:statsd][:port] = nil
