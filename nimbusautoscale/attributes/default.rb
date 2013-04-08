ac115c2329f3637b792162385e70b7738c652c95
Patrick Armstrong <patricka@uvic.ca>
2013-04-08 16:23:24 -0700

    Allow OpenTSDB host/port to be configured in Autoscale

--- nimbusautoscale/attributes/default.rb
+++ nimbusautoscale/attributes/default.rb
@@ -3,3 +3,5 @@ set_unless[:virtualenv][:virtualenv] = "virtualenv"
 set_unless[:autoscale][:exceptional_api_key] = nil
 set_unless[:autoscale][:statsd][:host] = nil
 set_unless[:autoscale][:statsd][:port] = nil
+set_unless[:autoscale][:opentsdb][:host] = "localhost"
+set_unless[:autoscale][:opentsdb][:port] = 4242
