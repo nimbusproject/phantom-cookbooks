34d9adc86b4063e2f74963af5c83f5d43da8a06d
Patrick Armstrong <patricka@uvic.ca>
2013-10-04 14:58:52 -0400

    Add configurable OpenTSDB host and port

--- phantomweb/attributes/default.rb
+++ phantomweb/attributes/default.rb
@@ -3,3 +3,5 @@ set_unless[:virtualenv][:virtualenv] = "virtualenv"
 set_unless[:phantomweb][:google_analytics_code] = ""
 set_unless[:phantomweb][:exceptional_api_key] = ""
 set_unless[:phantomweb][:install_config] = {}
+set_unless[:phantomweb][:opentsdb_host] = "localhost"
+set_unless[:phantomweb][:opentsdb_port] = 4242
