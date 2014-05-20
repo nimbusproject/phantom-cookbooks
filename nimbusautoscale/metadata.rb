321d927e45593faf72b7390e3b15478ace1dbd59
Pierre Riteau <priteau@uchicago.edu>
2014-05-20 15:33:06 +0100

    Explicitely depend on virtualenv
    This is required with Chef 11

---
+++ nimbusautoscale/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Pierre Riteau"
+maintainer_email "priteau@uchicago.edu"
+license          "Apache 2.0"
+description      "Installs/Configures nimbusautoscale"
+version          "0.0.1"
+depends          "virtualenv"
---
+++ supervisord/metadata.rb
@@ -0,0 +1,6 @@
+maintainer       "Pierre Riteau"
+maintainer_email "priteau@uchicago.edu"
+license          "Apache 2.0"
+description      "Installs/Configures supervisord"
+version          "0.0.1"
+depends          "virtualenv"
